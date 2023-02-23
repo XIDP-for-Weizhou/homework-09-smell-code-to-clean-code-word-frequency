470c5cada94c9e76c2752cc9cfcbd4674a0eb2cf
weizhou.zhang <weizhou.zhang@thoughtworks.com>
2023-02-24 00:30:49 +0800

    refactor: extract method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,26 +10,37 @@ import java.util.stream.Collectors;
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
         try {
-            List<String> allSingleWord = Arrays.stream(inputStr.split("\\s+")).distinct().collect(Collectors.toList());
-            List<Input> inputList = new ArrayList<>();
-
-            for (String word : allSingleWord) {
-                int count = (int) Arrays.stream(inputStr.split("\\s+")).filter(n -> n.equals(word)).count();
-                Input input = new Input(word, count);
-                inputList.add(input);
-            }
-
-            inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-            StringJoiner joiner = new StringJoiner("\n");
-            for (Input w : inputList) {
-                String s = w.getValue() + " " + w.getWordCount();
-                joiner.add(s);
-            }
-            return joiner.toString();
+            List<Input> inputList = getInputList(inputStr);
+            sortInputList(inputList);
+            return getResultString(inputList);
         } catch (
                 Exception e) {
             return "Calculate Error";
         }
+    }
+
+    private static String getResultString(List<Input> inputList) {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Input w : inputList) {
+            String s = w.getValue() + " " + w.getWordCount();
+            joiner.add(s);
+        }
+        return joiner.toString();
+    }
+
+    private static List<Input> getInputList(String inputStr) {
+        List<String> allSingleWord = Arrays.stream(inputStr.split("\\s+")).distinct().collect(Collectors.toList());
+        List<Input> inputList = new ArrayList<>();
+
+        for (String word : allSingleWord) {
+            int count = (int) Arrays.stream(inputStr.split("\\s+")).filter(n -> n.equals(word)).count();
+            Input input = new Input(word, count);
+            inputList.add(input);
+        }
+        return inputList;
+    }
+
+    private static void sortInputList(List<Input> inputList) {
+        inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
     }
 }
